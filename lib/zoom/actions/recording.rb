6777c94dabf0bab7c26a21c703595e42606cfbba
Josh Schwartzman <geeosh@gmail.com>
2020-02-22 06:42:18 -0800

    Add request headers to recording requests

--- lib/zoom/actions/recording.rb
+++ lib/zoom/actions/recording.rb
@@ -7,28 +7,28 @@ module Zoom
       def user_recording_list(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
         options.require(%i[user_id])
-        Utils.parse_response self.class.get("/users/#{options[:user_id]}/recordings", query: options)
+        Utils.parse_response self.class.get("/users/#{options[:user_id]}/recordings", query: options, headers: request_headers)
       end
 
       # Get all recordings for a meeting
       def meeting_recording_list(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
         options.require(%i[meeting_id])
-        Utils.parse_response self.class.get("/meetings/#{options[:meeting_id]}/recordings", query: options)
+        Utils.parse_response self.class.get("/meetings/#{options[:meeting_id]}/recordings", query: options, headers: request_headers)
       end
 
       # Delete all recordings for a meeting
       def meeting_recordings_delete(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
         options.require(%i[meeting_id])
-        Utils.parse_response self.class.delete("/meetings/#{options[:meeting_id]}/recordings", query: options)
+        Utils.parse_response self.class.delete("/meetings/#{options[:meeting_id]}/recordings", query: options, headers: request_headers)
       end
 
       # Delete a single recording for a meeting
       def meeting_recording_delete(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
         options.require(%i[meeting_id recording_id])
-        Utils.parse_response self.class.delete("/meetings/#{options[:meeting_id]}/recordings/#{options[:recording_id]}", query: options)
+        Utils.parse_response self.class.delete("/meetings/#{options[:meeting_id]}/recordings/#{options[:recording_id]}", query: options, headers: request_headers)
       end
     end
   end
